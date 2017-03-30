3629aab6318bcec32a44b8b0b80156f86a2c50f1
mtgao2 <michael.gao1@gmail.com>
2017-03-30 00:54:21 -0500

    added timestamp for messages

--- message.py
+++ message.py
@@ -1,6 +1,9 @@
+import time
+
 class Message: 
 	def __init__(self, type, memberlist, data):
 		self.header = type
+		self.timestamp = time.strftime('%H:%M:%S')
 		self.memberlist = memberlist
 		self.content = data
 	
